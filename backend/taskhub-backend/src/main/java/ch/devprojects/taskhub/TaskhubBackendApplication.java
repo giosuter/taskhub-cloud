a91d3e4f074877cdffd78f0b112ae3b8fddff6a8
giosuter <giovanni.suter@me.com>
2025-09-21 16:13:06 +0200

    Initial release

---
+++ backend/taskhub-backend/src/main/java/ch/devprojects/taskhub/TaskhubBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.devprojects.taskhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskhubBackendApplication.class, args);
+	}
+
+}
---
+++ backend/taskhub-backend/src/test/java/ch/devprojects/taskhub/TaskhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.devprojects.taskhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
